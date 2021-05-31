66cfbbc53d88240c52d94d65130f21a590fdd117
benji <benji_1992_@msn.com>
2021-05-31 19:19:48 +0200

    TOFIX fixed

--- src/Components/FavoList.js
+++ src/Components/FavoList.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import { View, ActivityIndicator } from 'react-native'
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
 import { useSelector } from "react-redux";
 import { FlatList } from "react-native-gesture-handler";
 import FavoItem from "./FavoItem";
 import EmptyList from "./EmptyList";
+import { styles } from "../Stylez/Styling";
 
-const FavoList = () => {
+const FavoList = () =>
+{
     const favoProducts = useSelector(state => state.manageFavos);
-    console.log(favoProducts)
-    const renderItem = ({ item }) => (<FavoItem productData={ item } itemId={ item.id }  />);
+    console.log(favoProducts);
+    const renderItem = ({ item }) => (<FavoItem productData={ item } itemId={ item.id } />);
     /**
      * get usestate
      */
     return (
-        <View>
-        <FlatList
-            data={ favoProducts }
-            renderItem={ renderItem }
-            keyExtractor={ item => item.id.toString() }
-            ListEmptyComponent={ EmptyList } />
+        <View style={ styles.container }>
+            <FlatList
+                showsVerticalScrollIndicator={ false }
+                data={ favoProducts }
+                renderItem={ renderItem }
+                keyExtractor={ item => item.id.toString() }
+                ListEmptyComponent={ EmptyList } />
         </View>
-    )
-}
+    );
+};
 
-export default FavoList
+export default FavoList;
