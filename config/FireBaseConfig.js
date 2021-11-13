6ad6b9ce8a8604cdaf3b8b5f4d7fbab8dc52ee56
Silassentinel <53788992+Silassentinel@users.noreply.github.com>
2021-11-13 15:08:52 +0100

    Update FireBaseConfig.js

--- config/FireBaseConfig.js
+++ config/FireBaseConfig.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyAQGOJILL_OCSjW8hT8VuMOp9O6pWBDOs8",
-    authDomain: "mobileproject-271d2.firebaseapp.com",
-    projectId: "mobileproject-271d2",
-    storageBucket: "mobileproject-271d2.appspot.com",
-    messagingSenderId: "953245568034",
-    appId: "1:953245568034:web:90f1859ec7d4068095ce7b",
-    measurementId: "G-J3LKETMNYF"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
